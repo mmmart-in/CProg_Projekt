fbf158b548bffbeb3bf5d2006bc8f542be684eef
henrysmitt <rickardsmitt@gmail.com>
2020-12-02 09:20:25 +0100

    test SDL

--- C++_Proj/C++_Proj/Main.cpp
+++ C++_Proj/C++_Proj/Main.cpp
@@ -10,6 +10,12 @@ int main(int argc, char** argv) {
 	
 	SDL_Init(SDL_INIT_EVERYTHING);
 
+	SDL_Window* win = SDL_CreateWindow("Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 200, 200, NULL);
+	SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
+	SDL_RenderPresent(ren);
+
+	SDL_Delay(2000);
+
 	std::cout<< "Hello world!";
 	std::cout << "git e skitt";
 
