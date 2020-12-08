473f164707144ebeebef3aacb6d754a0911dc80c
mmmart-in <63650287+mmmart-in@users.noreply.github.com>
2020-12-08 09:42:04 +0100

    MainWindow

--- C++_Proj/C++_Proj/Main.cpp
+++ C++_Proj/C++_Proj/Main.cpp
@@ -7,13 +7,6 @@ void berättaOmGit() {
 }
 
 int main(int argc, char** argv) {
-	
-	SDL_Init(SDL_INIT_EVERYTHING);
-
-	SDL_Window* win = SDL_CreateWindow("Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 200, 200, NULL);
-	SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
-	SDL_RenderPresent(ren);
-
 	SDL_Delay(2000);
 
 	//Pushar igen då, RICKARD!!!
--- C++_Proj/C++_Proj/MainWIndow.cpp
+++ C++_Proj/C++_Proj/MainWIndow.cpp
@@ -1,5 +1,24 @@
-#include "MainWIndow.h"
+#include "MainWindow.h"
 #include <SDL.h>
 
 
-//HEJHEJ
+MainWindow::MainWindow() 
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+	win = SDL_CreateWindow("Space Invaders", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1200, 800, 0);
+	ren = SDL_CreateRenderer(win, -1, 0);
+}
+
+MainWindow::~MainWindow() 
+{
+	SDL_DestroyWindow(win);
+	SDL_DestroyRenderer(ren);
+	SDL_Quit();
+}
+
+SDL_Renderer* MainWindow::get_ren() const 
+{
+	return ren;
+}
+
+MainWindow mainWindow;
