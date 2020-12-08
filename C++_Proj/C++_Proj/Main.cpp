8167d48e03de2516ade75e2035cf4878635281a0
mmmart-in <63650287+mmmart-in@users.noreply.github.com>
2020-12-08 11:43:16 +0100

    visa alien

--- C++_Proj/C++_Proj/Main.cpp
+++ C++_Proj/C++_Proj/Main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <SDL.h>
 #include "MainWindow.h"
+#include <SDL_image.h>
+#include "GameSystem.h"
 
 
 void berättaOmGit() {
@@ -8,22 +10,16 @@ void berättaOmGit() {
 }
 
 int main(int argc, char** argv) {
-	SDL_Delay(2000);
+
+	SDL_Surface* alien = IMG_Load("C:/resources/alien.png");
+	SDL_Texture* alienText = SDL_CreateTextureFromSurface(mainWindow.get_ren(), alien);
+	SDL_FreeSurface(alien);
+	SDL_RenderCopy(mainWindow.get_ren(), alienText, NULL, NULL);
+
 	SDL_RenderPresent(mainWindow.get_ren());
+	SDL_Delay(5000);
 
-	//Pushar igen då, RICKARD!!!
-
-	std::cout<< "Hello world!";
-	std::cout << "git e skitt";
-
-	std::cout << "WOW, skoj med SDL" << std::endl;
-
-	//I FIX
-	//ÄR RICKARDS LINKER KVAR??? 
-	// NUDÅ!?
-	//FUNKAR FÖR MIG MEGALULZ
-
-	berättaOmGit();
+	
 
 	return 0;
 }
