f10df533d9bf7fdb10802c44ff2cc00a964be683
mmmart-in <63650287+mmmart-in@users.noreply.github.com>
2020-12-08 10:34:19 +0100

    gamesys

--- C++_Proj/C++_Proj/GameSystem.h
+++ C++_Proj/C++_Proj/GameSystem.h
@@ -1,5 +1,18 @@
 #pragma once
+#include <vector>
+#include "Component.h"
 class GameSystem
 {
+	public:
+		void add_component(Component*);
+		void remove_component(Component*);
+		void remove_all_components();
+		void run();
+		~GameSystem();
+
+	private:
+		std::vector<Component*> components;
+
+
 };
 
