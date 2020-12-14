9097fb4b4f9fc22a4f3d8b2fd3ad07bace8dfb7c
mmmart-in <63650287+mmmart-in@users.noreply.github.com>
2020-12-14 09:58:40 +0100

    nestad for-loop

--- C++_Proj/C++_Proj/EnemyHandler.h
+++ C++_Proj/C++_Proj/EnemyHandler.h
@@ -7,9 +7,8 @@ class EnemyHandler
 public:
 	//konstruktor kanske ska ta rows+columns som argument istället för att göra det lättare senare
 	//vill man här bestämma width och height för hela raden? det går ju att göra.
-	EnemyHandler(int startX, int startY, int numOfEnemies);
+	EnemyHandler(int startX, int startY, int rows, int cols);
 private:
 	std::vector<Enemy> enemies;
-	int x, y;
 };
 
