d81f23b6898a5039cf83467d1b0051adc4ae3bc3
mmmart-in <63650287+mmmart-in@users.noreply.github.com>
2021-01-04 14:20:52 +0100

    Update AudioHandler.cpp

--- C++_Proj/C++_Proj/AudioHandler.cpp
+++ C++_Proj/C++_Proj/AudioHandler.cpp
@@ -4,18 +4,16 @@
 AudioHandler::AudioHandler(){
 	Mix_OpenAudio(20500, AUDIO_S16SYS, 2, 512);
 	Mix_Music* loop = Mix_LoadMUS("E:/Downloads/post rock bounce.mp3");
-	
-
+	enemyShoot = Mix_LoadWAV("../../Resources/SoundFX/SpaceInvaderShoot_Kort.wav");
+	playerShoot = Mix_LoadWAV("../../Resources/SoundFX/SpaceInvaders_PlayerShoot.wav");
 }
 
 void AudioHandler::enemy_shoot() {
-	enemyShoot = Mix_LoadWAV("../../Resources/SoundFX/SpaceInvaderShoot_Kort.wav");
 	if(Mix_PlayChannel(-1, enemyShoot, 0) ==-1)
 		std::cerr <<Mix_GetError()<< std::endl;
 	std::cout << "enemyshoot kallad" << std::endl;
 }
 void AudioHandler::player_shoot() {
-	playerShoot = Mix_LoadWAV("../../Resources/SoundFX/SpaceInvaders_PlayerShoot.wav");
 	Mix_PlayChannel(-1, playerShoot, 0);;
 	std::cout << "playershoot kallad"<< std::endl;
 }
